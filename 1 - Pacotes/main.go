b2bfff399fc5ffb47de6cfd5dcaf2e039bff9e7c
Pedro Grachet <pedro.grachet@gmail.com>
2023-06-05 15:51:15 -0400

    Pacotes externos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo algo qualquer na main")
 	auxiliar.Escrever()
 	auxiliar.Escrever2()
+
+	erro := checkmail.ValidateFormat("pedro.grachetgmail.com")
+	fmt.Println(erro)
 }
