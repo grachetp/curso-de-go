05a6c23e13d28bc0b7660c371d7b4f0ef0053225
Pedro Grachet <pedro.grachet@gmail.com>
2023-08-03 23:07:48 -0400

    channels buffer quantity

--- 18 - Goroutines e Concorrencia/4 - channels with buffers/channels-buffers.go
+++ 18 - Goroutines e Concorrencia/4 - channels with buffers/channels-buffers.go
@@ -6,11 +6,15 @@ import "fmt"
 //Uma função que envia o valor e outra que recebe
 
 func main() {
-	canal := make(chan string, 2) //Canal com capacidade de 2. Só vai bloquear a capacidade máxima do canal.
+	canal := make(chan string, 2) //Canal com capacidade de 2. Só vai bloquear quando estiver na capacidade máxima do canal.
+	//Sem buffer, já iria bloquear no primeiro envio
 
 	canal <- "Olá mundo!"
 	canal <- "Programando em Golang!"
 
 	mensagem := <-canal
+	fmt.Println(mensagem)
+
+	mensagem = <-canal
 	fmt.Println(mensagem)
 }
