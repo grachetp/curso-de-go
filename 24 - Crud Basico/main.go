9f9df1f2e9533563ab6d58c9fe5c29f608507e22
Pedro Grachet <pedro.grachet@gmail.com>
2023-12-03 16:11:42 -0400

    feat: added update to users

--- 24 - Crud Basico/main.go
+++ 24 - Crud Basico/main.go
@@ -12,7 +12,10 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/users", servidor.CreateUser).Methods(http.MethodPost)
+	router.HandleFunc("/users", servidor.FindUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", servidor.FindUser).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", servidor.UpdateUser).Methods(http.MethodPut)
 
 	fmt.Println("Listen on port 5000")
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe("127.0.0.1:5000", router))
 }
--- 24 - Crud Basico/servidor/servidor.go
+++ 24 - Crud Basico/servidor/servidor.go
@@ -5,7 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type user struct {
@@ -14,7 +18,14 @@ type user struct {
 	Email string `json:"email"`
 }
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
 func CreateUser(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	bodyRequest, err := io.ReadAll(r.Body)
 	if err != nil {
 		w.Write([]byte("Falha ao ler o corpo da requisição"))
@@ -55,4 +66,124 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", idInserted)))
+}
+
+func FindUser(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	parameters := mux.Vars(r)
+
+	ID, err := strconv.ParseUint(parameters["id"], 10, 32)
+	if err != nil {
+		w.Write([]byte("Erro ao converter o parametro ID para inteiro"))
+		return
+	}
+
+	db, err := banco.Connect()
+	if err != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+
+	line, err := db.Query("SELECT * FROM users WHERE id = ?", ID)
+	if err != nil {
+		w.Write([]byte("Erro ao buscar usuário no banco de dados"))
+		return
+	}
+
+	var user user
+	if line.Next() {
+		if err := line.Scan(&user.Id, &user.Name, &user.Email); err != nil {
+			w.Write([]byte("Erro ao escanear o usuário!"))
+			return
+		}
+	}
+
+	if user.Id == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		w.Write([]byte("Erro ao converter o usuário para JSON!"))
+		return
+	}
+}
+
+func FindUsers(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	db, err := banco.Connect()
+	if err != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	lines, err := db.Query("SELECT * FROM Users")
+	if err != nil {
+		w.Write([]byte("Erro ao buscar os usuários"))
+		return
+	}
+	defer lines.Close()
+
+	var users []user
+
+	for lines.Next() {
+		var user user
+
+		if err := lines.Scan(&user.Id, &user.Name, &user.Email); err != nil {
+			w.Write([]byte("Erro ao escanear o usuário"))
+			return
+		}
+
+		users = append(users, user)
+	}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		w.Write([]byte("Erro ao converter os usuários para JSON"))
+		return
+	}
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	parameters := mux.Vars(r)
+
+	ID, err := strconv.ParseUint(parameters["id"], 10, 32)
+	if err != nil {
+		w.Write([]byte("Erro ao converter o parametro ID para inteiro"))
+		return
+	}
+
+	bodyRequest, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.Write([]byte("Erro ao ler o corpo da requisição"))
+		return
+	}
+
+	var user user
+	if err := json.Unmarshal(bodyRequest, &user); err != nil {
+		w.Write([]byte("Erro ao converter o usuário para struct"))
+		return
+	}
+
+	db, err := banco.Connect()
+	if err != nil {
+		w.Write([]byte("Erro ao conectar no banco de dados"))
+		return
+	}
+
+	defer db.Close()
+
+	statement, err := db.Prepare("UPDATE USERS SET name = ?, email = ? where id = ?")
+	if err != nil {
+		w.Write([]byte("Erro ao criar o statement"))
+		return
+	}
+	defer statement.Close()
+
+	if _, err := statement.Exec(user.Name, user.Email, ID); err != nil {
+		w.Write([]byte("Erro ao atualizar o usuário"))
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
