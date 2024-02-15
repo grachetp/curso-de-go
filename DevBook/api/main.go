e8534527d6112e282bfeb39bd6a0a3b8f18d240f
Pedro Grachet <pedro.grachet@gmail.com>
2024-02-15 00:41:27 -0400

    variaveis de ambiente e configuracao de banco de dados

--- DevBook/api/main.go
+++ DevBook/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,10 @@ import (
 )
 
 func main() {
-	fmt.Println("API running!")
+	config.Carregar()
 
+	fmt.Println("API running!")
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
