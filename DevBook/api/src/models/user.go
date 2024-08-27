8d2172f97f003de30cc203c766b635b57d8d5571
Pedro Grachet <pedro.grachet@gmail.com>
2024-08-26 22:26:15 -0400

    chore: added package responses and validation data to insert in Database

--- DevBook/api/src/models/user.go
+++ DevBook/api/src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +13,39 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"CreatedAt,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("the name is required and not can be empty")
+	}
+
+	if user.Nick == "" {
+		return errors.New("the Nick is required and not can be empty")
+	}
+
+	if user.Email == "" {
+		return errors.New("the Email is required and not can be empty")
+	}
+
+	if user.Password == "" {
+		return errors.New("the Password is required and not can be empty")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
--- DevBook/api/src/router/controllers/users.go
+++ DevBook/api/src/router/controllers/users.go
@@ -4,36 +4,45 @@ import (
 	"api/src/database"
 	"api/src/models"
 	"api/src/repositories"
+	"api/src/responses"
 	"encoding/json"
-	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	bodyRequest, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		responses.Err(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 
 	var user models.User
 	if err = json.Unmarshal(bodyRequest, &user); err != nil {
-		log.Fatal(err)
+		responses.Err(w, http.StatusBadGateway, err)
+		return
+	}
+
+	if err := user.Prepare(); err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
 	}
 
 	db, err := database.Connect()
 	if err != nil {
-		log.Fatal(err)
+		responses.Err(w, http.StatusInternalServerError, err)
+		return
 	}
+	defer db.Close()
 
 	repository := repositories.NewUsersRepository(db)
-	userId, err := repository.Create(user)
+	user.ID, err = repository.Create(user)
 	if err != nil {
-		log.Fatal(err)
+		responses.Err(w, http.StatusInternalServerError, err)
+		return
 	}
-	w.Write([]byte(fmt.Sprintf("Inserted Id: %d", userId)))
-	//PAREI NA AULA 78 - 2:00
+
+	responses.JSON(w, http.StatusCreated, user)
 }
 
 func FindUsers(w http.ResponseWriter, r *http.Request) {
